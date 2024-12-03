08188a7e7cd228bb2f77c9c5e0b5e64d339093ba
Uwe Krause <uwe@uwekrau.se>
2024-12-03 21:18:13 +0100

    simplify server, actually just return sent data

--- server/server.py
+++ server/server.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
 def echo():
-    data = request.get_json()
-    return jsonify(data)
+    # ToDo: maybe actually do anything with the data to introduce a chance for a failure?
+    return request.data
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
